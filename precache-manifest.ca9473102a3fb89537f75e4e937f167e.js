fcaa368dece53e4f2ec84f1b2a2083cc28756d36
dwcy <dawid@bahnhof.se>
2018-11-28 23:40:42 +0100

    webbluetooth

---
+++ precache-manifest.ca9473102a3fb89537f75e4e937f167e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2f16e5ac83078df940b0",
+    "url": "/js/chunk-vendors.85799a4c.js"
+  },
+  {
+    "revision": "54994feb6acad4e180e4",
+    "url": "/js/app.9fa46306.js"
+  },
+  {
+    "revision": "5b223ce56d81fe07786aebe891e4ced0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54994feb6acad4e180e4",
+    "url": "/css/app.cab2071d.css"
+  }
+];
